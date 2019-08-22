06ce6f4b1271a4f9369683db5b34ae7264ae2d00
the-coinmaker <coinsdevcode@gmail.com>
2019-08-22 21:38:41 +0200

    update-v1003

--- update-ozeety-v1.0.0.3.sh
+++ update-ozeety-v1.0.0.3.sh
@@ -11,12 +11,13 @@ sudo rm -R -f ozeetyd
 sudo rm -R -f ozeety-cli 
 sudo rm -R -f ozeety-tx
 echo 25%
-sudo wget https://github.com/the-coinmaker/oztg/raw/master/linux/ozeetyd
 sudo wget https://github.com/the-coinmaker/oztg/raw/master/linux/ozeety-tx
+sudo wget https://github.com/the-coinmaker/oztg/raw/master/linux/ozeetyd
 sudo wget https://github.com/the-coinmaker/oztg/raw/master/linux/ozeety-cli
 sudo chmod +x ozeetyd
-sudo chmod +x ozeety-tx
 sudo chmod +x ozeety-cli
+sudo chmod +x ozeety-tx
+sudo mv ozeetyd ozeety-cli ozeety-tx /usr/bin/
 echo 50%
 cd .ozeety
   rm -R -f chainstate blocks .log mn.dat budget.dat peers.dat wallet.dat mncache.dat mnpayments.dat fee_estimates.dat db.log debug.log database/ backups/ ozeety.pid .lock
